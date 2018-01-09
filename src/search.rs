fcc258df838b1c3ca5b8892a5c5834629f3d9e71
Alexandru Pana <alexandru.pana@manabreakstudios.com>
2018-01-09 21:55:03 +0200

    Dummy 'match_abbrev' implementation

---
+++ src/search.rs
@@ -0,0 +1,55 @@
+pub fn match_abbrev(value: &str, query: &str) -> bool {
+
+    if value.to_lowercase().starts_with(query.to_lowercase().as_str()) {
+        return true;
+    }
+
+    let mut parts = Vec::new();
+    let mut part = String::from("");
+    for current_char in query.chars() {
+        if current_char.is_uppercase() {
+            if !part.is_empty() {
+                parts.push(part);
+            }
+            part = String::from("");
+        }
+        part.push(current_char);
+    }
+    if !part.is_empty() {
+        parts.push(part);
+    }
+
+    let mut index = 0;
+    for part in parts {
+        match value[index..].find(&part) {
+            None => return false,
+            Some(v) => {
+                index = index + v;
+            }
+        }
+    }
+
+    return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_match_abbrev_pass() {
+        assert_eq!(true, match_abbrev("ProjectileSpawnLocation", "ProjSpLoc"));
+        assert_eq!(true, match_abbrev("ProjectileSpawnLocation", "PSL"));
+        assert_eq!(true, match_abbrev("ProjectileSpawnLocation", "ProjS"));
+        assert_eq!(true, match_abbrev("ProjectileSpawnLocation", "ProLoc"));
+        assert_eq!(true, match_abbrev("ProjectileSpawnLocation", "SLoc"));
+        assert_eq!(true, match_abbrev("ProjectileSpawnLocation", "P"));
+        assert_eq!(true, match_abbrev("ProjectileSpawnLocation", "proj"));
+    }
+
+    #[test]
+    fn test_match_abbrev_fail() {
+        assert_eq!(false, match_abbrev("ProjectileSpawnLocation", "ProjSpLc"));
+        assert_eq!(false, match_abbrev("ProjectileSpawnLocation", "PSLC"));
+    }
+}
